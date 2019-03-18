6a73feeb9d5ca6b7db9810b72a1482ed7cfa3e98
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-18 07:50:38 -0400

    Created my first method

--- src/com/Lhschools/Main.java
+++ src/com/Lhschools/Main.java
@@ -3,6 +3,13 @@ package com.Lhschools;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printAnswer("Messi!");
+        printAnswer("Ronaldo!");
+        printAnswer("Neymar!");
+
+    }
+
+    public static void printAnswer(String noun){
+        System.out.println("The best soccer player in the world is " +noun);
     }
 }
