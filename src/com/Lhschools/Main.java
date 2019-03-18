06d2517ab9093573b88d42553f7f1505970dc313
Bharat <bharat.paudel.2019@student.lhschools.org>
2019-03-18 08:00:12 -0400

    Created my second method

--- src/com/Lhschools/Main.java
+++ src/com/Lhschools/Main.java
@@ -3,13 +3,20 @@ package com.Lhschools;
 public class Main {
 
     public static void main(String[] args) {
+
         printAnswer("Messi!");
         printAnswer("Ronaldo!");
         printAnswer("Neymar!");
 
+        printWeather("Lovely.");
+        printWeather("Beautiful.");
+        printWeather("Miserable.");
     }
 
     public static void printAnswer(String noun){
         System.out.println("The best soccer player in the world is " +noun);
+    }
+    public static void printWeather(String noun){
+        System.out.println("Hello there, today's weather looks quite " +noun);
     }
 }
