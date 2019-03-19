b41ad0dd24e333412d74c7b00f951a32aaa6e9dd
Bharat107 <47633551+Bharat107@users.noreply.github.com>
2019-03-18 23:02:04 -0400

    Update Main.java
    Cleaned and finished up Methods

--- src/com/Lhschools/Main.java
+++ src/com/Lhschools/Main.java
@@ -11,28 +11,46 @@ public class Main {
         printWeather("Lovely.");
         printWeather("Beautiful.");
         printWeather("Miserable.");
-        
+
         int a = 3;
         int b = 6;
         System.out.println(a + " times " + b + " is ");
         System.out.println(times(a, b));
-        
+
         printWorld("World");
         printWorld("Universe");
-        printWorld("Galaxy");    
+        printWorld("Galaxy");
+
+        int c = 100;
+        int d = 10;
+        System.out.println(c + " divided by " + d + " is ");
+        System.out.println(Divide(d, c));
+
+        System.out.println(+ d + " goes into " + c + " ten times, the modulus of " + c + " by " + d + " is ");
+        System.out.println(Modulus(d, c));
     }
 
-    public static void printAnswer(String noun){
-        System.out.println("Hello " +noun);
+    public static void printAnswer(String noun) {
+        System.out.println("Hello " + noun);
     }
-    public static void printWeather(String noun){
-        System.out.println("Hello there, today's weather looks quite " +noun);
+
+    public static void printWeather(String noun) {
+        System.out.println("Hello there, today's weather looks quite " + noun);
     }
-    public static int times(int firstNumber, int secondNumber){
+
+    public static int times(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
-    public static void printWorld(String noun){
-        System.out.println("Hello " +noun);
+
+    public static void printWorld(String noun) {
+        System.out.println("Hello " + noun);
+    }
+
+    public static int Modulus(int firstNumber, int secondNumber) {
+        return secondNumber % firstNumber;
+    }
+
+    public static int Divide(int firstNumber, int secondNumber) {
+        return secondNumber / firstNumber;
     }
 }
-        
