5f9ea4f0609e70b39570d041fa87e1448dcbe6d1
Bharat107 <47633551+Bharat107@users.noreply.github.com>
2019-03-18 21:03:03 -0400

    Update Main.java
    Changed first method

--- src/com/Lhschools/Main.java
+++ src/com/Lhschools/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        printAnswer("Messi!");
-        printAnswer("Ronaldo!");
-        printAnswer("Neymar!");
+        printAnswer("Messi");
+        printAnswer("Ronaldo");
+        printAnswer("Neymar");
 
         printWeather("Lovely.");
         printWeather("Beautiful.");
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static void printAnswer(String noun){
-        System.out.println("The best soccer player in the world is " +noun);
+        System.out.println("Hello " +noun);
     }
     public static void printWeather(String noun){
         System.out.println("Hello there, today's weather looks quite " +noun);
